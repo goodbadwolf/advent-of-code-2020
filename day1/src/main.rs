2ca91fd1f009d2ca7f8bd03767a8ded8b6f3748f
Manish Mathai <manishpmathai@gmail.com>
2020-12-02 17:26:08 -0800

    Day 1 second half solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,28 @@
 use std::fs;
 
+const EXPENSES_SUM: i32 = 2020;
+
 fn parse_input(input: &String) -> Vec<i32> {
     input.lines().map(|i| i.parse::<i32>().unwrap()).collect()
 }
 
-fn solve(inputs: Vec<i32>) -> (i32, i32) {
-    let lhs_result = *(inputs
+fn find_two_expenses(inputs: &Vec<i32>, sum: i32) -> Option<(i32, i32)> {
+    let expenses = inputs
         .iter()
-        .find(|&&i| inputs.iter().position(|&j| j == 2020 - i).is_some())
-        .unwrap());
-    return (lhs_result, (2020 - lhs_result));
+        .find(|&&i| inputs.iter().position(|&j| j == sum - i).is_some());
+    match expenses {
+        Some(&lhs_expense) => Some((lhs_expense, sum - lhs_expense)),
+        None => None,
+    }
+}
+
+fn find_three_expenses(inputs: &Vec<i32>, sum: i32) -> Option<(i32, i32, i32)> {
+    inputs
+        .iter()
+        .find_map(|&i| match find_two_expenses(inputs, sum - i) {
+            Some((middle_expense, right_expense)) => Some((i, middle_expense, right_expense)),
+            None => None,
+        })
 }
 
 fn main() {
@@ -17,12 +30,22 @@ fn main() {
 
     let inputs = parse_input(&inputs);
 
-    let (lhs_expense, rhs_expense) = solve(inputs);
+    if let Some((lhs_expense, rhs_expense)) = find_two_expenses(&inputs, EXPENSES_SUM) {
+        println!(
+            "{} x {} = {}",
+            lhs_expense,
+            rhs_expense,
+            (lhs_expense * rhs_expense)
+        );
+    }
 
-    println!(
-        "{} x {} = {}",
-        lhs_expense,
-        rhs_expense,
-        (lhs_expense * rhs_expense)
-    );
+    if let Some((l_expense, m_expense, r_expense)) = find_three_expenses(&inputs, EXPENSES_SUM) {
+        println!(
+            "{} x {} x {} = {}",
+            l_expense,
+            m_expense,
+            r_expense,
+            l_expense * m_expense * r_expense
+        );
+    }
 }
